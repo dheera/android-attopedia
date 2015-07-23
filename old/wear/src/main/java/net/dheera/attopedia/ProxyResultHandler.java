ce65b59757d8fd669b72fa33b64f2ffb71325217
Dheera Venkatraman <dheera@dheera.net>
2015-07-23 03:32:31 +0000

    rebasing because of silly Gradle update that breaks all previously-written code

--- old/wear/src/main/java/net/dheera/attopedia/ProxyResultHandler.java
+++
@@ -1,9 +0,0 @@
-package net.dheera.attopedia;
-
-/**
- * Methods to asynchronously deal with the output of ProxyClient.
- */
-public class ProxyResultHandler {
-    public void onResult(byte[] data) { /* to be overridden */ }
-    public void onFail() { /* to be overridden */ }
-}
